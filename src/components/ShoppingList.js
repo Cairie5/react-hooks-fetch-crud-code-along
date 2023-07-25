389f032f27b18056acc205b513f9ffc78f8c6da5
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-07-25 10:21:15 +0300

    Completed Code along

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState,useEffect
+ } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -11,26 +12,56 @@ function ShoppingList() {
     setSelectedCategory(category);
   }
 
+  useEffect(() => {
+    fetch("http://localhost:4000/items")
+      .then((r) => r.json())
+      .then((items) => setItems(items));
+  }, []);
+
+  function handleDeleteItem(deletedItem) {
+    const updatedItems = items.filter((item) => item.id !== deletedItem.id);
+  setItems(updatedItems);
+  }
+
+  function handleUpdateItem(updatedItem) {
+    const updatedItems = items.map((item) => {
+      if (item.id === updatedItem.id) { 
+        return updatedItem;
+      } else {
+        return item;
+      }
+    });
+    setItems(updatedItems);
+  }
+
+  function handleAddItem(newItem) {
+  setItems([...items, newItem]);
+}
+
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
   });
-
   return (
-    <div className="ShoppingList">
-      <ItemForm />
-      <Filter
-        category={selectedCategory}
-        onCategoryChange={handleCategoryChange}
-      />
-      <ul className="Items">
-        {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
-        ))}
-      </ul>
-    </div>
-  );
+      <div className="ShoppingList">
+        <ItemForm onAddItem={handleAddItem} />
+        <Filter
+          category={selectedCategory}
+          onCategoryChange={handleCategoryChange}
+        />
+        <ul className="Items">
+          {itemsToDisplay.map((item) => (
+            <Item
+              key={item.id}
+              item={item}
+              onUpdateItem={handleUpdateItem}
+              onDeleteItem={handleDeleteItem}
+            />
+          ))}
+        </ul>
+      </div>
+    );
 }
 
 export default ShoppingList;
